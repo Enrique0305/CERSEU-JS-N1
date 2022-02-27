d0bc36c43da6662583c3fecc72d0763920f1a6a7
Enrique Chalco <enrique.chalco@unmsm.edu.pe>
2022-02-27 09:41:11 -0500

    se agrego los types en JS fundamentos

---
+++ Fundamentos/types.js
@@ -0,0 +1,25 @@
+// Tipos primitivos
+// number 
+40
+//string
+"enrique"
+'chalco'
+//typeof es un tipo de operador unitario que trabaja con un operador (variable);
+var nombre;
+
+typeof 30
+'number'
+typeof true
+'boolean'
+typeof 'enrique'
+'string'
+typeof ""
+'string'
+typeof {}
+'object'
+typeof []
+'object'
+typeof null
+'object'
+typeof undefined
+'undefined'
