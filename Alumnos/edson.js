00444aabcd05a4c311496dd9f7cbab5331dd40bd
enrique miguel chalco vila <enrique.chalco@unmsm.edu.pe>
2022-05-24 20:17:10 -0500

    cambios en los directorios 24/05/2022

---
+++ Alumnos/edson.js
@@ -0,0 +1,50 @@
+// Autor: Edson prudencio
+// reto 1 y 2
+
+function juego(pc,usu){
+if (pc === usu) {
+      console.log("es un empate");
+    }
+else if((pc === "piedra" && usu === "tijera") ||
+        (pc === "papel" && usu === "piedra") ||
+        (pc === "tijera" && usu === "papel")){
+      
+        
+      
+        console.log("pc gana");
+      }
+     else{ console.log("usuario gana");
+         }
+    }
+juego("piedra","papel")
+
+
+
+
+function juegos2(PC,USU){
+  if (PC === USU) return "Empate";
+
+  switch (PC) {
+    case "PIEDRA":
+      switch (USU) {
+        case "PAPEL":
+          return "Ganaste";
+        default:
+          return "PC gana";
+      }
+    case "PAPEL":
+      switch (USU) {
+        case "TIJERA":
+          return "Ganaste";
+        default:
+          return "PC gana";
+      }
+    case "TIJERA":
+      switch (USU) {
+        case "PIEDRA":
+          return "Ganaste";
+        default:
+          return "PC gana";
+      }
+  }
+}
